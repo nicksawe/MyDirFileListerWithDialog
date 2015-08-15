24568ebd894dfa318357951e7f22129594ed48fb
Nick Sawe <nicksawe@users.noreply.github.com>
2015-08-15 10:57:05 +0100

    WinForm to select dir and save the filenames to a txt file

---
+++ MyDirFileListerWithDialog/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace MyDirFileListerWithDialog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSource = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnDestination = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblSource = new System.Windows.Forms.Label();
+            this.lblDestination = new System.Windows.Forms.Label();
+            this.txtFilename = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSource
+            // 
+            this.btnSource.Location = new System.Drawing.Point(29, 13);
+            this.btnSource.Name = "btnSource";
+            this.btnSource.Size = new System.Drawing.Size(164, 23);
+            this.btnSource.TabIndex = 0;
+            this.btnSource.Text = "Select Source Directory";
+            this.btnSource.UseVisualStyleBackColor = true;
+            this.btnSource.Click += new System.EventHandler(this.btnSource_Click);
+            // 
+            // btnDestination
+            // 
+            this.btnDestination.Location = new System.Drawing.Point(29, 43);
+            this.btnDestination.Name = "btnDestination";
+            this.btnDestination.Size = new System.Drawing.Size(164, 23);
+            this.btnDestination.TabIndex = 1;
+            this.btnDestination.Text = "Select Destination Directory";
+            this.btnDestination.UseVisualStyleBackColor = true;
+            this.btnDestination.Click += new System.EventHandler(this.btnDestination_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(199, 79);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 2;
+            // 
+            // lblSource
+            // 
+            this.lblSource.AutoSize = true;
+            this.lblSource.Location = new System.Drawing.Point(199, 18);
+            this.lblSource.Name = "lblSource";
+            this.lblSource.Size = new System.Drawing.Size(0, 13);
+            this.lblSource.TabIndex = 3;
+            // 
+            // lblDestination
+            // 
+            this.lblDestination.AutoSize = true;
+            this.lblDestination.Location = new System.Drawing.Point(199, 48);
+            this.lblDestination.Name = "lblDestination";
+            this.lblDestination.Size = new System.Drawing.Size(0, 13);
+            this.lblDestination.TabIndex = 4;
+            // 
+            // txtFilename
+            // 
+            this.txtFilename.Location = new System.Drawing.Point(29, 73);
+            this.txtFilename.Name = "txtFilename";
+            this.txtFilename.Size = new System.Drawing.Size(164, 20);
+            this.txtFilename.TabIndex = 5;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(29, 108);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 6;
+            this.btnOK.Text = "GO";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(118, 108);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "CANCEL";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 168);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtFilename);
+            this.Controls.Add(this.lblDestination);
+            this.Controls.Add(this.lblSource);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnDestination);
+            this.Controls.Add(this.btnSource);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSource;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnDestination;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblSource;
+        private System.Windows.Forms.Label lblDestination;
+        private System.Windows.Forms.TextBox txtFilename;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ MyDirFileListerWithDialog/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MyDirFileListerWithDialog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string Path ;
+        public bool IsSelected ;
+        public List<string> Files ;
+        public int Filecount;
+        public DateTime Start ;
+        public DateTime End ;
+        public string PathToWriteTo ;
+
+        private void btnSource_Click(object sender, EventArgs e)
+        {
+            GetValue();
+            lblSource.Text = Path;
+            GetFileData(out Files, out Filecount, out Start, out End);
+        }
+
+        private void GetValue()
+        {
+            IsSelected = false;
+            folderBrowserDialog1.SelectedPath = "";
+            folderBrowserDialog1.ShowDialog();
+            Path = folderBrowserDialog1.SelectedPath;
+
+            if (Path != string.Empty)
+            {
+                IsSelected = true;
+               // MessageBox.Show(path);
+            }
+        }
+
+        private void btnDestination_Click(object sender, EventArgs e)
+        {
+            GetValue();
+            lblDestination.Text = Path;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (Control control in Controls)
+            {
+                if (control is Label)
+                {
+                    control.Text = "";
+                }
+            }
+        }
+
+        private void GetFileData(out List<string> Files, out int Filecount, out DateTime Start, out DateTime End)
+        {
+            Start = DateTime.Now;
+
+            var temp = Directory.EnumerateFiles(Path, "*.*", SearchOption.AllDirectories).ToList();
+            temp.Sort();
+            Files = temp;
+
+            Filecount = Files.Count();
+
+            End = DateTime.Now;
+        }
+
+        public void WriteToFile()
+        {
+            
+            using (var x = File.CreateText(Path + @"\" + txtFilename.Text + ".txt"))
+            {
+                foreach (var t in Files)
+                {
+                    x.Write(t + Environment.NewLine);
+                }
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (txtFilename.Text!="")
+            {
+                WriteToFile();
+                lblResult.Text = Filecount + @" files written to " + Path + @"\" + txtFilename.Text + @".txt";
+            }
+            else
+            {
+                lblResult.Text = @"Enter filename to save to";
+            }
+
+            
+        }
+    }
+}
+
+
